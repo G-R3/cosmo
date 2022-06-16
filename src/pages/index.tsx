cf0ea1a6aaa596ea45d36cd3add77cef2a741940
Gerardo <gr261212@gmail.com>
2022-06-16 19:37:44 -0400

    create and show posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,73 +1,52 @@
 import type { NextPage } from "next";
-import Image from "next/image";
 import { trpc } from "../utils/trpc";
 import Navbar from "../components/Navbar";
 
 const Home: NextPage = () => {
+  const postQuery = trpc.useQuery(["post.all"]);
+
+  if (postQuery.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (postQuery.error) {
+    return <div>Error grabbing posts</div>;
+  }
+
+  const { data: posts } = postQuery;
+
   return (
     <>
       <Navbar />
       <section className="flex flex-col items-center gap-10">
-        <div className="w-full max-w-3xl bg-white rounded-md p-5">
-          <h2 className="text-xl font-semibold">This is My Post</h2>
-          <span className="flex gap-2 mb-3">
-            <small>Posted by</small>
-            <small>Tuxedoed</small>
-            <small>10 hrs ago</small>
-          </span>
-          {/* <Image width={256} height={256} /> */}
-          {/* <div className="h-96 w-full bg-gray-300 rounded-md"></div> */}
-          <div className="truncate">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
-            excepturi, quibusdam laborum assumenda veritatis odit unde voluptas
-            necessitatibus dolorem expedita deserunt? A et repellat autem!
-            Voluptate soluta earum pariatur ea officia in, sed quidem veritatis
-            quo suscipit ipsum ipsam ullam doloremque eum quisquam tempore
-            officiis iure dolorem. Corporis, illo ipsa quidem eveniet temporibus
-            tenetur delectus maxime sapiente assumenda harum pariatur eaque
-            perspiciatis architecto adipisci, omnis officia, accusantium amet
-            nobis necessitatibus! Ratione incidunt nisi officiis numquam.
-            Adipisci aspernatur sequi maxime molestiae eligendi atque fugit eius
-            est nulla cum exercitationem qui doloremque commodi, non assumenda
-            necessitatibus mollitia nemo reiciendis explicabo quisquam
-            molestias!
-          </div>
-          <div className="flex justify-between mt-3">
-            <div className="flex justify-center items-center gap-2">
-              <button>Upvote</button>
-              <span>123</span>
-              <button>Downvote</button>
-            </div>
-            <div className="flex gap-5">
-              <button>Share</button>
-              <button>Save</button>
-              <button>100 Comment</button>
+        {posts?.map((post) => (
+          <div
+            key={post.id}
+            className="w-full max-w-3xl bg-white rounded-md p-5"
+          >
+            <h2 className="text-xl font-semibold">{post.title}</h2>
+            <span className="flex gap-2 mb-3">
+              <small>Posted by</small>
+              <small>Tuxedoed</small>
+              <small>10 hrs ago</small>
+            </span>
+            {/* <Image width={256} height={256} /> */}
+            {/* <div className="h-96 w-full bg-gray-300 rounded-md"></div> */}
+            <div className="truncate">{post.content}</div>
+            <div className="flex justify-between mt-3">
+              <div className="flex justify-center items-center gap-2">
+                <button>Upvote</button>
+                <span>123</span>
+                <button>Downvote</button>
+              </div>
+              <div className="flex gap-5">
+                <button>Share</button>
+                <button>Save</button>
+                <button>100 Comment</button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="w-full max-w-3xl bg-white rounded-md p-5">
-          <h2 className="text-xl font-semibold">This is My Post</h2>
-          <span className="flex gap-2 mb-3">
-            <small>Posted by</small>
-            <small>Tuxedoed</small>
-            <small>10 hrs ago</small>
-          </span>
-          {/* <Image width={256} height={256} /> */}
-          <div className="h-96 w-full bg-gray-300 rounded-md"></div>
-
-          <div className="flex justify-between mt-3">
-            <div className="flex justify-center items-center gap-2">
-              <button>Upvote</button>
-              <span>123</span>
-              <button>Downvote</button>
-            </div>
-            <div className="flex gap-5">
-              <button>Share</button>
-              <button>Save</button>
-              <button>Comment</button>
-            </div>
-          </div>
-        </div>
+        ))}
       </section>
     </>
   );
--- src/pages/submit/index.tsx
+++ src/pages/submit/index.tsx
@@ -1,21 +1,45 @@
 import type { NextPage } from "next";
+import { useState } from "react";
+import { trpc } from "../../utils/trpc";
 
 const Submit: NextPage = () => {
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+
+  const mutation = trpc.useMutation("post.create");
+
+  const handleSubmit = () => {
+    mutation.mutate({ title, content });
+  };
+
   return (
     <section className="max-w-xl mx-auto">
       <h1 className="text-2xl font-semibold my-10">Create Post</h1>
       <div className="bg-white p-5 flex flex-col gap-2 rounded-md">
+        {mutation.error && (
+          <p className="bg-red-400 text-white p-3 rounded-md">
+            Something went wrong!
+          </p>
+        )}
         <input
           type="text"
           placeholder="Title"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
           className="w-full py-2 px-3 border-2 rounded-md"
         />
         <textarea
           placeholder="Text (optional)"
+          onChange={(e) => setContent(e.target.value)}
+          value={content}
           className="w-full py-2 px-3 border-2 rounded-md"
         ></textarea>
 
-        <button className="bg-blue-500 text-white rounded-md p-2 self-end">
+        <button
+          onClick={handleSubmit}
+          disabled={mutation.isLoading}
+          className="bg-blue-500 text-white rounded-md p-2 self-end"
+        >
           Post
         </button>
       </div>
