87992e51da7eb53c3a44929a2b093f1f1b040cb6
Gerardo <gr261212@gmail.com>
2022-06-16 13:03:34 -0400

    add tailwind and src dir

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document = () => {
+  return (
+    <Html>
+      <Head />
+      <body className="bg-neutral-900">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
