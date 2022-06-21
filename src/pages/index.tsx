bed7761c1f1ae56e029d8cf1878e8e46a0adc59d
Gerardo <gr261212@gmail.com>
2022-06-21 11:55:25 -0400

    create grid layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import type { NextPage } from "next";
-import Link from "next/link";
 import { trpc } from "../utils/trpc";
 
-import Header from "../components/Header";
-import Markdown from "../components/Markdown";
+import Post from "../components/Post";
+import { useSession } from "next-auth/react";
+import UserCard from "../components/UserCard";
 
 const Home: NextPage = () => {
   const postQuery = trpc.useQuery(["post.all"], {
     staleTime: 1000 * 60 * 60 * 24, // 24 hours in ms
   });
+  const { data: session } = useSession();
 
   if (postQuery.isLoading) {
     return <div>Loading...</div>;
@@ -22,37 +23,28 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <section className="flex flex-col items-center gap-10">
-        {posts?.map((post) => (
-          <div
-            key={post.id}
-            className="bg-neutral border-2 border-transparent hover:border-accent w-full max-w-3xl rounded-md p-5 transition-all"
-          >
-            <h2 className="text-xl font-semibold">{post.title}</h2>
-            <span className="flex gap-2 mb-3">
-              <small>Posted by</small>
-              <small>Tuxedoed</small>
-              <small>10 hrs ago</small>
-            </span>
-            {/* <Image width={256} height={256} /> */}
-            {/* <div className="h-96 w-full bg-gray-300 rounded-md"></div> */}
-            {/* <div className="truncate">{post.content}</div> */}
-            <div className="ellipsis">
-              <Markdown content={post.content ? post.content : ""} />
-            </div>
-            <div className="flex justify-between mt-3">
-              <div className="flex justify-center items-center gap-2">
-                <button>Upvote</button>
-                <span>Vote</span>
-                <button>Downvote</button>
-              </div>
-
-              <Link href={`/post/${post.slug}`}>
-                <a>100 Comment</a>
-              </Link>
-            </div>
+      <section className="grid grid-cols-5 gap-x-5">
+        <div>
+          <div className="bg-neutral rounded-md sticky top-20">
+            <h1 className="text-center">Todo Add something here</h1>
           </div>
-        ))}
+        </div>
+        <div className="col-span-3 flex flex-col items-center gap-10">
+          {posts?.map((post) => (
+            <Post
+              key={post.id}
+              id={post.id}
+              title={post.title}
+              content={post.content}
+              slug={post.slug}
+            />
+          ))}
+        </div>
+        {!!session?.user && (
+          <div>
+            <UserCard {...session?.user} />
+          </div>
+        )}
       </section>
     </>
   );
