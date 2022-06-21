f58352553857dff2187529e44faa3f69b3b0e890
Gerardo <gr261212@gmail.com>
2022-06-20 22:24:17 -0400

    fix the flickering by adding some transition

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,25 +2,51 @@ import Link from "next/link";
 import { useSession, signIn, signOut, getProviders } from "next-auth/react";
 
 const Navbar: React.FC = () => {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
 
   return (
-    <nav className="p-3 px-5">
-      <ul className="flex gap-5 justify-end">
-        <li>
-          <Link href="/submit">
-            <a className="p-3">Create Post</a>
-          </Link>
-        </li>
-        <li>
-          {session ? (
-            <button onClick={() => signOut()}>Sign out</button>
-          ) : (
-            <button onClick={() => signIn()}>Sign in</button>
-          )}
-        </li>
-      </ul>
-    </nav>
+    <header
+      className={`backdrop-saturate-50 backdrop-blur-sm fixed top-0 left-0 w-full z-10 transition-all duration-200 ease-in ${
+        !session && loading ? "-top-8 opacity-0" : "top-0 opacity-1"
+      }`}
+    >
+      <div className="relative px-8 h-14 lg:h-16 ">
+        <div className="flex">
+          <div className="flex-shrink-0">
+            <Link href={"/"}>
+              <a className="flex items-center h-14 lg:h-16 w-full text-3xl">
+                Cosmo
+              </a>
+            </Link>
+          </div>
+          <div className="flex gap-5 justify-end items-center flex-grow">
+            <nav className={`flex gap-5`}>
+              <Link href="/submit">
+                <a className="flex items-center h-14 lg:h-16">Create Post</a>
+              </Link>
+
+              {session ? (
+                <button
+                  onClick={() => signOut()}
+                  className="flex items-center h-14 lg:h-16"
+                >
+                  Sign out
+                </button>
+              ) : (
+                <button
+                  onClick={() => signIn()}
+                  className="flex items-center h-14 lg:h-16"
+                >
+                  Sign in
+                </button>
+              )}
+            </nav>
+            <div>{session?.user && <p>{session.user.name}</p>}</div>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
 
