a0bc9a4e65e501294cd0b45bcd48b89cfb37b1e5
Gerardo <gr261212@gmail.com>
2022-06-24 13:26:53 -0400

    style signin page

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,6 +1,12 @@
 import { getProviders, signIn } from "next-auth/react";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { useRouter } from "next/router";
+import { AiFillGithub } from "react-icons/ai";
+
+const providerButtonStyles: Record<string, string> = {
+  github: "bg-zinc-800 text-secondary hover:bg-zinc-700",
+  google: "bg-blue-700 text-white hover:bg-blue-600",
+};
 
 const SignIn = ({
   providers,
@@ -8,20 +14,25 @@ const SignIn = ({
   const callbackUrl = useRouter().query.callbackUrl as string;
 
   return (
-    <>
-      {Object.values(providers).map((provider: any) => (
-        <div key={provider.name}>
+    <div className="flex flex-col items-center justify-center max-w-xl mx-auto">
+      <h1 className="text-3xl font-bold text-center mb-10">Log in to Cosmo</h1>
+      <div className="max-w-xs">
+        {Object.values(providers).map((provider: any) => (
           <button
-            className="btn"
+            key={provider.id}
+            className={`btn w-full mt-3 focus:outline-accent ${
+              providerButtonStyles[provider.id]
+            }`}
             onClick={() =>
               signIn(provider.id, { callbackUrl: callbackUrl ?? "/" })
             }
           >
+            <AiFillGithub size={25} className="mr-2" />
             Sign in with {provider.name}
           </button>
-        </div>
-      ))}
-    </>
+        ))}
+      </div>
+    </div>
   );
 };
 
