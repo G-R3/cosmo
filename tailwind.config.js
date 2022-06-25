d25426a23699d9d35093a9b2c520849cbd4cc9ab
Gerardo <gr261212@gmail.com>
2022-06-24 23:21:22 -0400

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      background: "#000",
+      accentOne: "#111",
+      accentTwo: "#333",
+      accentFour: "#EAEAEA",
+      foreground: "#fff",
+
+      highlight: "#8b5cf6",
+      error: "##F33",
+      success: "#0070F3",
+      warning: "#F5A623",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
