a6f66c9e96b136ab134d6f9cabb97a4474446722
Gerardo <gr261212@gmail.com>
2022-06-25 12:31:09 -0400

    update skeleton styles

--- src/components/PostSkeleton.tsx
+++ src/components/PostSkeleton.tsx
@@ -9,25 +9,24 @@ const PostSkeleton = () => {
   return (
     <motion.div
       variants={variants}
-      className="bg-neutral border-2 border-transparent hover:border-accent w-full rounded-md p-5 transition-all max-h-[168px]"
+      className="bg-accentOne w-full rounded-md p-5 "
     >
-      <div className="animate-pulse">
-        <h2 className="text-xl font-semibold bg-neutral-content brightness-50 rounded-md h-6 w-full"></h2>
-        <span className="flex gap-2 mt-2 mb-3 h-2 w-48 bg-neutral-content brightness-50 rounded-md"></span>
+      <div>
+        <h2 className="bg-accentTwo bg-skeleton bg-[length:400%_100%] animate-skeletonLoading rounded-md h-5 w-full"></h2>
+        <span className="flex gap-2 mt-2 mb-3 h-2 w-48 bg-accentTwo bg-skeleton bg-[length:400%_100%] animate-skeletonLoading rounded-sm"></span>
         <div className="flex flex-col gap-2 my-4">
-          <p className="h-2 w-full bg-neutral-content brightness-50 rounded-md"></p>
-          <p className="h-2 w-full bg-neutral-content brightness-50 rounded-md"></p>
-          <p className="h-2 w-full bg-neutral-content brightness-50 rounded-md"></p>
-          {/* <Markdown content={content ? content : ""} /> */}
+          <p className="h-3 w-full bg-accentTwo bg-skeleton bg-[length:400%_100%] animate-skeletonLoading rounded-sm"></p>
+          <p className="h-3 w-full bg-accentTwo bg-skeleton bg-[length:400%_100%] animate-skeletonLoading rounded-sm"></p>
+          <p className="h-3 w-full bg-accentTwo bg-skeleton bg-[length:400%_100%] animate-skeletonLoading rounded-sm"></p>
         </div>
         <div className="flex justify-between mt-3">
           <div className="flex justify-center items-center gap-2">
-            <button className="h-5 w-12 bg-neutral-content brightness-50 rounded-md"></button>
-            <span className="h-5 w-12 bg-neutral-content brightness-50 rounded-md"></span>
-            <button className="h-5 w-12 bg-neutral-content brightness-50 rounded-md"></button>
+            <span className="h-5 w-12 bg-accentTwo bg-skeleton bg-[length:400%_100%] animate-skeletonLoading rounded-md"></span>
+            <span className="h-5 w-12 bg-accentTwo bg-skeleton bg-[length:400%_100%] animate-skeletonLoading rounded-md"></span>
+            <span className="h-5 w-12 bg-accentTwo bg-skeleton bg-[length:400%_100%] animate-skeletonLoading rounded-md"></span>
           </div>
 
-          <span className="h-5 w-36 bg-neutral-content brightness-50 rounded-md"></span>
+          <span className="h-5 w-36 bg-accentTwo bg-skeleton bg-[length:400%_100%] animate-skeletonLoading rounded-md"></span>
         </div>
       </div>
     </motion.div>
