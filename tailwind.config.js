190c94ea769cfe295eb402a743e91d862abc70ac
Gerardo <gr261212@gmail.com>
2022-06-24 22:14:25 -0400

    remove daisyui and add headlessui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,29 +4,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"), require("@tailwindcss/typography")],
-  daisyui: {
-    themes: [
-      {
-        light: {
-          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
-          neutral: "#f3f4f6",
-        },
-      },
-      {
-        dark: {
-          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
-          primary: "#f43f5e",
-          secondary: "#f3f4f6",
-          accent: "#8b5cf6",
-          neutral: "#1c1917",
-          "base-100": "#000000",
-          info: "#38bdf8",
-          success: "#36D399",
-          warning: "#FBBD23",
-          error: "#f43f5e",
-        },
-      },
-    ],
-  },
+  plugins: [require("@tailwindcss/typography")],
 };
