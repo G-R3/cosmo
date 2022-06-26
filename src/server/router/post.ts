74e414b4b840fb436075b5eb1068ef34c95f346e
Gerardo <gr261212@gmail.com>
2022-06-26 00:00:12 -0400

    add user data with post data

--- src/server/router/post.ts
+++ src/server/router/post.ts
@@ -20,9 +20,30 @@ export const postRouter = createRouter()
   })
   .query("all", {
     async resolve() {
-      const posts = await prisma.post.findMany();
+      const posts = await prisma.post.findMany({
+        include: {
+          user: true,
+        },
+      });
 
-      return posts;
+      // doing this so that I can exclude the userEmail field idk why I'm doing https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/working-with-many-to-many-relations#explicit-relations
+
+      // but I can also do it like this: https://www.prisma.io/docs/concepts/components/prisma-client/relation-queries#select-specific-relation-fields
+      return [
+        ...posts.map((post) => ({
+          id: post.id,
+          title: post.title,
+          content: post.content,
+          slug: post.slug,
+          createdAt: post.createdAt,
+          updatedAt: post.updatedAt,
+          user: {
+            id: post.user.id,
+            name: post.user.name,
+            image: post.user.image,
+          },
+        })),
+      ];
     },
   })
   .mutation("create", {
@@ -37,11 +58,13 @@ export const postRouter = createRouter()
           message: "You are not authorized",
         });
       }
+
       await prisma.post.create({
         data: {
           title: input.title,
           content: input.content,
           slug: input.title.toLowerCase().replace(/\s/g, "-"),
+          userEmail: ctx.session.user.email!,
         },
       });
 
