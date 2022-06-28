78807e20812e28f865bb5eb576c0fe2e454e7930
Gerardo <gr261212@gmail.com>
2022-06-28 14:36:58 -0400

    removed user endpoint

--- src/server/router/_app.ts
+++ src/server/router/_app.ts
@@ -1,13 +1,12 @@
 import { createRouter } from "../createRouter";
 import { commentRouter } from "./comment";
 import { postRouter } from "./post";
-import { userRouter } from "./user";
+
 import { voteRouter } from "./vote";
 
 export const appRouter = createRouter()
   .merge("post.", postRouter)
   .merge("comment.", commentRouter)
-  .merge("vote.", voteRouter)
-  .merge("user.", userRouter);
+  .merge("vote.", voteRouter);
 
 export type AppRouter = typeof appRouter;
