0fe4f24bff6916a7162544d61a277ab5b490a943
Gerardo <gr261212@gmail.com>
2022-07-02 18:29:52 -0400

    search for a community

--- src/server/router/community.ts
+++ src/server/router/community.ts
@@ -3,19 +3,38 @@ import { z } from "zod";
 import { prisma } from "../../db/client";
 import { TRPCError } from "@trpc/server";
 
-export const communityRouter = createRouter().mutation("create", {
-  input: z.object({
-    name: z.string().trim().min(1),
-    description: z.string().nullable(),
-  }),
-  async resolve({ input }) {
-    const community = await prisma.community.create({
-      data: {
-        name: input.name,
-        description: input.description,
-      },
-    });
+export const communityRouter = createRouter()
+  .query("search", {
+    input: z.object({
+      query: z.string().trim(),
+    }),
+    async resolve({ input }) {
+      if (!input.query) return;
 
-    return community;
-  },
-});
+      const communities = await prisma.community.findMany({
+        where: {
+          name: {
+            contains: input.query,
+          },
+        },
+      });
+
+      return communities;
+    },
+  })
+  .mutation("create", {
+    input: z.object({
+      name: z.string().trim().min(1),
+      description: z.string().nullable(),
+    }),
+    async resolve({ input }) {
+      const community = await prisma.community.create({
+        data: {
+          name: input.name,
+          description: input.description,
+        },
+      });
+
+      return community;
+    },
+  });
