4b1a0552a9f0e470951a8905075cd508108de009
Gerardo <gr261212@gmail.com>
2022-07-08 17:19:22 -0400

    create community page and create routes

---
+++ src/pages/c/[community]/[...post]/index.tsx
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+const Index = () => {
+  const router = useRouter();
+
+  return <div>A post</div>;
+};
+
+export default Index;
