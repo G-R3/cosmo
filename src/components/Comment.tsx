4fd778607b10db0269cabad77d37fafb9270db20
Gerardo <gr261212@gmail.com>
2022-07-13 17:50:56 -0400

    handle comment edit/delete

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,5 +1,12 @@
+import useTextarea from "@/hooks/useTextarea";
+import { trpc } from "@/utils/trpc";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { useState } from "react";
+import { FiEdit2 } from "react-icons/fi";
 import Markdown from "./Markdown";
+import MarkdownTipsModal from "./MarkdownTipsModal";
+import CommentDeleteModal from "./CommentDeleteModal";
 
 interface Props {
   id: number;
@@ -14,15 +21,33 @@ interface Props {
 }
 const Comment: React.FC<Props> = ({
   id,
-  content,
+  content: commentBody,
   createdAt,
   updatedAt,
   author,
 }) => {
+  const { data: session } = useSession();
+  const { content, setContent, textareaRef } = useTextarea(commentBody, 150);
+  const [isEditing, setIsEditing] = useState(false);
+  const utils = trpc.useContext();
+  const commentEditMutation = trpc.useMutation("comment.edit", {
+    onSuccess(data, variables, context) {
+      utils.invalidateQueries([
+        "comment.get-by-postId",
+        { postId: data.comment.postId },
+      ]);
+      setIsEditing(false);
+    },
+  });
+
+  const handleEdit = (commentId: number, newCommentBody: string) => {
+    commentEditMutation.mutate({ commentId, content: newCommentBody });
+  };
+
   return (
     <div className="bg-whiteAlt flex gap-5 dark:bg-darkOne py-3 px-5 rounded-md">
       <div className="bg-foreground dark:bg-darkTwo min-h-full min-w-[4px] rounded-full"></div>
-      <div className="flex flex-col gap-5 flex-grow">
+      <div className="flex flex-col flex-grow">
         <div className="flex items-center gap-3">
           <Image
             src={author?.image ?? ""}
@@ -35,9 +60,53 @@ const Comment: React.FC<Props> = ({
           <span className="text-grayAlt">{author.name}</span>
           {/* <span>{createdAt}</span> */}
         </div>
-        <div className="mb-5">
-          <Markdown content={content} />
+        <div className="mt-5 mb-3">
+          {isEditing ? (
+            <div className="flex flex-col gap-2 mb-3">
+              <MarkdownTipsModal />
+              <textarea
+                data-cy="comment-edit-textarea"
+                ref={textareaRef}
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                name="comment"
+                id="comment"
+                placeholder="What are you thoughts?"
+                className="py-3 px-4 border-2 focus:outline-none focus:border-grayAlt dark:focus:border-grayAlt rounded-md bg-whiteAlt dark:border-darkTwo text-darkTwo placeholder:text-grayAlt dark:bg-darkOne dark:text-foreground overflow-hidden min-h-[150px] resize-none"
+              ></textarea>
+            </div>
+          ) : (
+            <Markdown content={commentBody} />
+          )}
         </div>
+
+        {author.id === session?.user.id && (
+          <div className="flex justify-end gap-5 items-center">
+            <CommentDeleteModal commentId={id} />
+            <button
+              data-cy="comment-edit"
+              onClick={() => setIsEditing((prev) => !prev)}
+              className="py-1 px-2 flex items-center gap-[6px] text-grayAlt hover:text-blue-400 focus:text-blue-400"
+            >
+              <FiEdit2 />
+              {isEditing ? "Cancel Edit" : "Edit"}
+            </button>
+            {isEditing && (
+              <button
+                data-cy="save-comment-edit"
+                onClick={() => handleEdit(id, content)}
+                disabled={
+                  commentEditMutation.isLoading ||
+                  commentBody === content ||
+                  !content
+                }
+                className="bg-success text-whiteAlt self-end h-12 p-4 rounded-md flex items-center disabled:opacity-50 animate-popIn active:hover:animate-none active:focus:animate-none active:focus:scale-95 active:hover:scale-95 transition-all"
+              >
+                Save
+              </button>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
---
+++ src/components/CommentDeleteModal.tsx
@@ -0,0 +1,81 @@
+import { trpc } from "@/utils/trpc";
+import { FC, useState } from "react";
+import { FiTrash2 } from "react-icons/fi";
+import { Dialog } from "@headlessui/react";
+import { AnimatePresence, motion } from "framer-motion";
+
+const DeleteCommentModal: FC<{ commentId: number }> = ({ commentId }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const utils = trpc.useContext();
+  const deleteMutation = trpc.useMutation("comment.delete", {
+    onSuccess(data, variables, context) {
+      setIsOpen(false);
+      utils.invalidateQueries("comment.get-by-postId");
+    },
+  });
+
+  const onDelete = (commentId: number) => {
+    deleteMutation.mutate({ commentId });
+  };
+
+  return (
+    <>
+      <button
+        data-cy="comment-delete"
+        disabled={deleteMutation.isLoading}
+        onClick={() => setIsOpen(true)}
+        className="flex items-center gap-2 text-grayAlt hover:text-alert"
+      >
+        <FiTrash2 />
+        Delete
+      </button>
+      <AnimatePresence>
+        {isOpen && (
+          <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, transition: { duration: 0.1 } }}
+              exit={{ opacity: 0, transition: { duration: 0.2 } }}
+              className="fixed inset-0 bg-background py-10 bg-opacity-80 z-10"
+            >
+              <div data-cy="delete-modal" className="flex justify-center p-4">
+                <Dialog.Panel
+                  as={motion.div}
+                  initial={{ opacity: 0, y: -100 }}
+                  animate={{ opacity: 1, y: 0, transition: { type: "tween" } }}
+                  exit={{ opacity: 0, y: -100, transition: { duration: 0.2 } }}
+                  className="flex flex-col w-full max-w-xl rounded bg-white dark:bg-darkOne px-10 py-8"
+                >
+                  <Dialog.Title className="text-2xl">Delete Post</Dialog.Title>
+                  <Dialog.Description className="mt-2">
+                    Deleting a comment can&apos;t be undone. Are you sure you
+                    want to continue?
+                  </Dialog.Description>
+
+                  <div className="mt-5 self-end flex gap-2">
+                    <button
+                      onClick={() => setIsOpen(false)}
+                      className="px-5 py-2 rounded-md"
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      data-cy="confirm-delete-comment"
+                      disabled={deleteMutation.isLoading}
+                      onClick={() => onDelete(commentId)}
+                      className="px-5 py-2 bg-red-500 rounded-md"
+                    >
+                      {deleteMutation.isLoading ? "Deleting..." : "Delete"}
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </div>
+            </motion.div>
+          </Dialog>
+        )}
+      </AnimatePresence>
+    </>
+  );
+};
+
+export default DeleteCommentModal;
