e18d33aa0a589108740b7f502dadc256b12844b2
Gerardo <gr261212@gmail.com>
2022-07-14 15:18:51 -0400

    fix delete button text color

--- src/components/DeletePostModal.tsx
+++ src/components/DeletePostModal.tsx
@@ -30,7 +30,7 @@ const DeletePostModal: FC<{ postId: number }> = ({ postId }) => {
         data-cy="post-delete"
         disabled={deleteMutation.isLoading}
         onClick={() => setIsOpen(true)}
-        className="py-1 px-2 border-2 text-foreground border-alert rounded-md flex items-center gap-[6px] disabled:opacity-50 animate-popIn active:hover:animate-none active:focus:animate-none active:focus:scale-95 active:hover:scale-95 transition-all"
+        className="py-1 px-2 border-2 dark:text-foreground border-alert rounded-md flex items-center gap-[6px] disabled:opacity-50 animate-popIn active:hover:animate-none active:focus:animate-none active:focus:scale-95 active:hover:scale-95 transition-all"
       >
         <FiTrash2 />
         Delete
@@ -70,7 +70,7 @@ const DeletePostModal: FC<{ postId: number }> = ({ postId }) => {
                       data-cy="confirm-delete-post"
                       disabled={deleteMutation.isLoading}
                       onClick={() => onDelete(postId)}
-                      className="px-5 py-2 bg-red-500 rounded-md"
+                      className="px-5 py-2 bg-red-500 rounded-md text-foreground"
                     >
                       {deleteMutation.isLoading ? "Deleting..." : "Delete"}
                     </button>
