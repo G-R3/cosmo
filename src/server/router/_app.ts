49baf1e9ec40bc3b6171d7b1d9a3fecd42658286
Gerardo <gr261212@gmail.com>
2022-07-20 17:40:37 -0400

    add user routers

--- src/server/router/_app.ts
+++ src/server/router/_app.ts
@@ -2,10 +2,12 @@ import { createRouter } from "../createRouter";
 import { commentRouter } from "./comment";
 import { communityRouter } from "./community";
 import { postRouter } from "./post";
+import { userRouter } from "./user";
 
 export const appRouter = createRouter()
   .merge("post.", postRouter)
   .merge("comment.", commentRouter)
-  .merge("community.", communityRouter);
+  .merge("community.", communityRouter)
+  .merge("user.", userRouter);
 
 export type AppRouter = typeof appRouter;
---
+++ src/server/router/user.ts
@@ -0,0 +1,117 @@
+import { createRouter } from "../createRouter";
+import { z } from "zod";
+import { TRPCError } from "@trpc/server";
+import { prisma } from "../../db/client";
+import { basePost } from "./post";
+
+export const userRouter = createRouter()
+  .query("get-by-id", {
+    input: z.object({
+      userId: z.string(),
+    }),
+    async resolve({ input, ctx }) {
+      const user = await prisma.user.findUnique({
+        where: {
+          id: input.userId,
+        },
+      });
+
+      return {
+        user,
+      };
+    },
+  })
+  .query("get-posts", {
+    input: z.object({
+      user: z.string(),
+    }),
+
+    async resolve({ input }) {
+      const posts = await prisma.post.findMany({
+        where: {
+          authorId: input.user,
+        },
+        select: {
+          ...basePost,
+          slug: true,
+          _count: {
+            select: {
+              comments: true,
+            },
+          },
+        },
+      });
+
+      return {
+        posts: [
+          ...posts.map((post) => ({
+            ...post,
+            commentCount: post._count.comments,
+          })),
+        ],
+      };
+    },
+  })
+  .query("get-liked-posts", {
+    input: z.object({
+      user: z.string(),
+    }),
+    async resolve({ input }) {
+      const posts = await prisma.post.findMany({
+        where: {
+          likes: {
+            some: {
+              userId: input.user,
+            },
+          },
+        },
+        select: {
+          ...basePost,
+          slug: true,
+          _count: {
+            select: {
+              comments: true,
+            },
+          },
+        },
+      });
+
+      return {
+        posts: [
+          ...posts.map((post) => ({
+            ...post,
+            commentCount: post._count.comments,
+          })),
+        ],
+      };
+    },
+  })
+  .query("get-comments", {
+    input: z.object({
+      user: z.string(),
+    }),
+    async resolve({ input }) {
+      const comments = await prisma.comment.findMany({
+        where: {
+          authorId: input.user,
+        },
+        select: {
+          id: true,
+          content: true,
+          createdAt: true,
+          updatedAt: true,
+          author: {
+            select: {
+              id: true,
+              name: true,
+              image: true,
+            },
+          },
+        },
+      });
+
+      return {
+        comments,
+      };
+    },
+  });
