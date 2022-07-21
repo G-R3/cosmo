3a7bd78fb94fa8fc1af260ec8dd938eb08399116
Gerardo <gr261212@gmail.com>
2022-07-21 14:49:05 -0400

    popover animation

--- src/components/UserBanner.tsx
+++ src/components/UserBanner.tsx
@@ -2,6 +2,7 @@ import { FC, useState } from "react";
 import Image from "next/image";
 import { BsGear } from "react-icons/bs";
 import { Popover } from "@headlessui/react";
+import { AnimatePresence, motion } from "framer-motion";
 
 type Props = {
   imageSrc: string;
@@ -54,7 +55,7 @@ const colorArray = [
 ];
 
 const BannerBackground = () => {
-  const [color, setColor] = useState(() => {
+  const [banneColor, setBannerColor] = useState(() => {
     const color = localStorage.getItem("bannerColor");
     try {
       return color ? `${JSON.parse(color)}` : "ef4444";
@@ -65,46 +66,68 @@ const BannerBackground = () => {
   });
 
   const changeBannerColor = (color: string) => {
-    setColor(color);
+    setBannerColor(color);
     localStorage.setItem("bannerColor", JSON.stringify(color));
   };
-
-  console.log(color);
 
   return (
     <>
       <div
-        className={`h-44 w-full absolute top-0 left-0 rounded-md flex justify-end items-start`}
+        className={`h-44 w-full absolute top-0 left-0 rounded-md flex justify-end items-start transition-colors duration-300 py-3 px-5`}
         style={{
-          backgroundColor: `#${color}`,
+          backgroundColor: `#${banneColor}`,
         }}
       >
         <Popover className="relative">
-          <Popover.Button className="p-2 group">
-            <span className="sr-only">Menu</span>
-            <BsGear
-              size={20}
-              className="group-hover:rotate-180 transition-transform duration-700"
-            />
-          </Popover.Button>
+          {({ open }) => (
+            <>
+              <Popover.Button className="group">
+                <span className="sr-only">Menu</span>
+                <BsGear
+                  size={20}
+                  className="group-hover:rotate-180 transition-transform duration-700 text-foreground dark:text-darkOne"
+                />
+              </Popover.Button>
 
-          <Popover.Panel className="absolute right-0 z-10 w-screen max-w-[250px]">
-            <div className="bg-darkOne grid grid-cols-3 p-2 rounded-md w-full">
-              {colorArray.map((color) => (
-                <button
-                  key={color.value}
-                  onClick={() => changeBannerColor(color.value)}
-                  className="text-sm flex flex-col gap-1 items-center rounded-md hover:cursor-pointer hover:bg-darkTwo py-2 px-5"
-                >
-                  <div
-                    style={{ backgroundColor: `#${color.value}` }}
-                    className="w-7 h-7 rounded-md"
-                  ></div>
-                  {color.name}
-                </button>
-              ))}
-            </div>
-          </Popover.Panel>
+              <AnimatePresence>
+                {open && (
+                  <Popover.Panel
+                    static
+                    className="absolute right-0 z-10 w-screen max-w-[250px]"
+                  >
+                    <motion.div
+                      initial={{ opacity: 0, scale: 0.8 }}
+                      animate={{ opacity: 1, scale: 1 }}
+                      exit={{
+                        opacity: 0,
+                        scale: 0.8,
+                        transition: { duration: 0.2 },
+                      }}
+                      className="shadow-md bg-whiteAlt dark:bg-darkOne grid grid-cols-3 gap-1 p-2 rounded-md w-full origin-top-right"
+                    >
+                      {colorArray.map((color) => (
+                        <button
+                          key={color.value}
+                          onClick={() => changeBannerColor(color.value)}
+                          className={`text-sm flex flex-col gap-1 items-center rounded-md hover:cursor-pointer hover:bg-foreground dark:hover:bg-darkTwo py-2 px-5 ${
+                            color.value === banneColor
+                              ? "bg-foreground dark:bg-darkTwo"
+                              : ""
+                          }`}
+                        >
+                          <div
+                            style={{ backgroundColor: `#${color.value}` }}
+                            className="w-7 h-7 rounded-md"
+                          ></div>
+                          {color.name}
+                        </button>
+                      ))}
+                    </motion.div>
+                  </Popover.Panel>
+                )}
+              </AnimatePresence>
+            </>
+          )}
         </Popover>
       </div>
     </>
