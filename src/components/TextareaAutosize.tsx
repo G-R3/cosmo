2f8315a06c0580e142aef674f992930fb29dd342
Gerardo <gr261212@gmail.com>
2022-07-23 22:16:12 -0400

    update auto grow textarea

--- src/components/TextareaAutosize.tsx
+++ src/components/TextareaAutosize.tsx
@@ -1,27 +1,42 @@
-import { ComponentPropsWithRef } from "react";
-import useAutosize from "@/hooks/useAutosize";
+import { ChangeEvent, ComponentPropsWithRef, useRef } from "react";
 
 type customProps = {
-  minHeight?: number;
+  register: any;
 };
 type TextareaProps = customProps & ComponentPropsWithRef<"textarea">;
 
 const TextareaAutosize = ({
-  value,
   className,
-  minHeight = 200,
+  register,
+  onChange = () => {},
   ...props
 }: TextareaProps) => {
-  const { textareaRef } = useAutosize(value, minHeight);
-
+  const { ref, onChange: postOnChange, ...rest } = register("postContent");
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
   const baseStyles = `py-3 px-4 border-2 focus:outline-none focus:border-grayAlt dark:focus:border-grayAlt rounded-md bg-whiteAlt dark:border-darkTwo text-darkTwo placeholder:text-grayAlt dark:bg-darkOne dark:text-foreground overflow-hidden resize-none`;
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "inherit";
+      const scrollHeight = textareaRef.current.scrollHeight;
+
+      textareaRef.current.style.height = `${Math.max(scrollHeight, 200)}px`;
+    }
+
+    postOnChange(e);
+    onChange(e);
+  };
 
   return (
     <textarea
       spellCheck="false"
-      ref={textareaRef}
-      value={value}
+      ref={(e) => {
+        ref(e);
+        textareaRef.current = e;
+      }}
       className={`${baseStyles} ${className ? className : ""}`}
+      onChange={handleChange}
+      {...rest}
       {...props}
     ></textarea>
   );
