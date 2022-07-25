c062f9c043aed179986e29dc36ca2092bc608fa8
Gerardo <gr261212@gmail.com>
2022-07-25 19:01:17 -0400

    moved to getSeverSideSession

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,9 +1,16 @@
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
-import { getSession } from "next-auth/react";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "src/pages/api/auth/[...nextauth]";
 
 export async function createContext(opts?: trpcNext.CreateNextContextOptions) {
-  const session = await getSession({ req: opts?.req });
+  if (!opts?.req || !opts.res) return null;
+
+  const session = await unstable_getServerSession(
+    opts.req,
+    opts.res,
+    authOptions,
+  );
 
   return {
     req: opts?.req,
