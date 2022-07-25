4148ec34d6dce7ec558b56aa63afd023c1f3a594
Gerardo <gr261212@gmail.com>
2022-07-25 19:06:29 -0400

    return context to avoid ctx being undefined

--- src/server/context.ts
+++ src/server/context.ts
@@ -3,19 +3,23 @@ import * as trpcNext from "@trpc/server/adapters/next";
 import { unstable_getServerSession } from "next-auth";
 import { authOptions } from "src/pages/api/auth/[...nextauth]";
 
-export async function createContext(opts?: trpcNext.CreateNextContextOptions) {
-  if (!opts?.req || !opts.res) return null;
-
-  const session = await unstable_getServerSession(
-    opts.req,
-    opts.res,
-    authOptions,
-  );
+export async function createContext(ctx?: trpcNext.CreateNextContextOptions) {
+  if (ctx?.req && ctx?.res) {
+    const session = await unstable_getServerSession(
+      ctx.req,
+      ctx.res,
+      authOptions,
+    );
+    return {
+      req: ctx?.req,
+      res: ctx?.res,
+      session,
+    };
+  }
 
   return {
-    req: opts?.req,
-    res: opts?.res,
-    session,
+    req: ctx?.req,
+    res: ctx?.res,
   };
 }
 
