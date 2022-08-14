e7df50497aa15027a647c1d8dfe45ed6ffbd3961
Gerardo <gr261212@gmail.com>
2022-08-13 20:46:47 -0400

    add ghost button option

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,22 +1,54 @@
 import { FC, ComponentPropsWithRef } from "react";
 
 interface ButtonProps extends ComponentPropsWithRef<"button"> {
-  variant?: "primary" | "secondary" | "danger" | "warning" | "success";
+  variant?:
+    | "default"
+    | "primary"
+    | "secondary"
+    | "danger"
+    | "warning"
+    | "success";
+
   size?: "sm" | "md" | "lg";
   icon?: React.ReactNode;
   loading?: boolean;
   fullWidth?: boolean;
+  ghost?: boolean;
 }
 
 const classes = {
   base: "relative rounded-md flex justify-center items-center gap-2 text-sm font-medium transition-colors duration-200",
-  disabled: "opacity-50 cursor-not-allowed",
   size: {
     sm: "h-8 min-h-[32px] min-w-[60px] py-1 px-4",
     md: "h-10 min-h-[32px] min-w-[60px] py-1 px-4",
     lg: "h-12 min-h-[32px] min-w-[60px] py-1 px-4",
   },
-  variants: {
+};
+
+const getAltButtonColors = (variant: ButtonProps["variant"]) => {
+  const altColors = {
+    default: "border border-darkTwo",
+    primary:
+      "text-highlight border border-highlight hover:text-white hover:bg-highlight active:bg-highlightActive",
+    secondary:
+      "border border-secondary hover:text-white hover:bg-secondary active:bg-secondaryActive",
+    success: "border border-success hover:bg-success active:bg-successActive",
+    danger:
+      "text-alert border border-alert hover:text-white hover:bg-alert active:bg-alertActive",
+    warning:
+      "text-warning border border-warning hover:text-white hover:bg-warning active:bg-warningActive",
+  };
+
+  return altColors[variant || "default"];
+};
+
+const getButtonColors = (
+  variant: ButtonProps["variant"],
+  ghost: boolean,
+  disabled: boolean,
+) => {
+  const colors = {
+    default: "",
     primary:
       "bg-highlight text-white hover:bg-highlightHover active:bg-highlightActive",
     secondary:
@@ -26,27 +58,39 @@ const classes = {
     danger: "bg-alert text-white hover:bg-alertHover active:bg-alertActive",
     warning:
       "bg-warning text-white hover:bg-warningHover active:bg-warningActive",
-  },
+  };
+
+  if (disabled) {
+    return "border border-grayAlt opacity-30 cursor-not-allowed";
+  }
+
+  if (ghost) {
+    return getAltButtonColors(variant);
+  }
+
+  return colors[variant || "default"];
 };
 
 const Button: FC<ButtonProps> = ({
-  variant = "primary",
+  variant = "default",
   size = "sm",
   children,
   icon,
   loading,
   fullWidth,
-  className,
-  disabled,
-  ...prop
+  ghost = false,
+  disabled = false,
+  ...props
 }) => {
+  const buttonStyles = getButtonColors(variant, ghost, disabled);
+
   return (
     <button
-      className={`${classes.base} ${classes.size[size]} ${
-        classes.variants[variant]
-      } ${disabled ? classes.disabled : ""} ${fullWidth ? "w-full" : "w-auto"}`}
       disabled={disabled || loading}
-      {...prop}
+      className={`${buttonStyles} ${classes.base} ${classes.size[size]} ${
+        fullWidth ? "w-full" : "w-auto"
+      }`}
+      {...props}
     >
       {icon && <span className="flex justify-center items-center">{icon}</span>}
       {loading ? "Loading..." : children}
