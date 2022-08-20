75b8efe0c1f04eba373001dbd8cb33ea9c6771cd
Gerardo <gr261212@gmail.com>
2022-08-20 12:48:52 -0400

    add url assertion

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -33,7 +33,8 @@ describe("smoke test", () => {
     cy.get("[data-cy='post-title']").type(postTitle);
     cy.get("[data-cy='post-body']").type(postBody);
     cy.get("[data-cy='submit']").click().should("be.disabled");
-    cy.get("[data-cy='post-title']").should("have.text", postTitle);
+    cy.url().should("include", `/${postSlug}`);
+    cy.get("[data-cy='post-title']").should("contain", postTitle);
     cy.get("[data-cy='post-community']").should("contain.text", communityName);
 
     // like/unlike post
