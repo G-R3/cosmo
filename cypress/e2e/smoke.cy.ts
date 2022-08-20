2ee59587c91d8330ecc439b1d52026c6c80eb4f7
Gerardo <gr261212@gmail.com>
2022-08-20 12:57:51 -0400

    why am i so bad at writing these tests?

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -12,7 +12,9 @@ describe("smoke test", () => {
     cy.wait("@session");
 
     // create community
-    cy.get("[data-cy='create-community-modal']").click({ force: true });
+    cy.get("[data-cy='create-community-modal']").click({
+      waitForAnimations: false,
+    });
     cy.get("[data-cy=community-name]").type(communityName);
     cy.get("[data-cy=confirm-create]").click();
 
