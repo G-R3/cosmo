f8a6fc71dac3bbd1d3a52cf6e4c7d03ff3b5469e
Gerardo <gr261212@gmail.com>
2022-09-02 16:55:24 -0400

    infer endpoint output type

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,4 +1,23 @@
 import { createReactQueryHooks } from "@trpc/react";
 import type { AppRouter } from "../backend/router/_app";
+import type { inferProcedureOutput, inferProcedureInput } from "@trpc/server";
 
 export const trpc = createReactQueryHooks<AppRouter>();
+
+export type inferQueryOutput<
+  TRouteKey extends keyof AppRouter["_def"]["queries"],
+> = inferProcedureOutput<AppRouter["_def"]["queries"][TRouteKey]>;
+
+export type inferQueryInput<
+  TRouteKey extends keyof AppRouter["_def"]["queries"],
+> = inferProcedureInput<AppRouter["_def"]["queries"][TRouteKey]>;
+
+export type inferMutationOutput<
+  TRouteKey extends keyof AppRouter["_def"]["mutations"],
+> = inferProcedureOutput<AppRouter["_def"]["mutations"][TRouteKey]>;
+
+export type inferMutationInput<
+  TRouteKey extends keyof AppRouter["_def"]["mutations"],
+> = inferProcedureInput<AppRouter["_def"]["mutations"][TRouteKey]>;
+
+export type UserCommunities = inferQueryOutput<"user.get-communities">;
